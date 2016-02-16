bbd4fb72f9b4835f3852682b3f5232c980463981
卷毛不爱下雨天 <244339217@qq.com>
2016-02-16 11:25:40 +0800

    Initial Commit

---
+++ MenuChoice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MenuChoice
+//
+//  Created by juanmao on 16/2/16.
+//  Copyright © 2016年 juanmao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
